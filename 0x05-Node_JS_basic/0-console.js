dc97fd78c416b21889562c66ee99510ed3227fcd
kellyinfortech <kellyinfortech.solution@gmail.com>
2024-06-18 10:07:54 -0700

    node js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -2,8 +2,8 @@
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
  */
-function displayMessage(data) {
-  console.log(data);
-}
+const displayMessage = (msg){
+  console.log(msg);
+};
 
 module.exports = displayMessage;
